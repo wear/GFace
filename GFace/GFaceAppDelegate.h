db79b408943efb1c9d5b318ddec28b28be931116
konglingliang <konglingliang@corp.the9.com>
2012-05-28 11:19:08 +0800

    Initial Commit

---
+++ GFace/GFaceAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GFaceAppDelegate.h
+//  GFace
+//
+//  Created by  on 12-5-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GFaceAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GFace/GFaceViewController.h
@@ -0,0 +1,13 @@
+//
+//  GFaceViewController.h
+//  GFace
+//
+//  Created by  on 12-5-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GFaceViewController : UIViewController
+
+@end
